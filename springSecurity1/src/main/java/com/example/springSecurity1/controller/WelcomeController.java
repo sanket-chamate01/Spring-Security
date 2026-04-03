dd376dc7eed9078aa7e16c45ec6be5db9275f319
sanket-chamate01 <sanket.chamate2001@gmail.com>
2026-04-03 17:20:45 +0530

    first commit

---
+++ springSecurity1/src/main/java/com/example/springSecurity1/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.example.springSecurity1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome(){
+        return "Welcome to JP Morgan Bank!";
+    }
+}
