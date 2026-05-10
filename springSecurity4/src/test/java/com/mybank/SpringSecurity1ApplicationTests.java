851938cb1ba74d9bd16c9c15bb179e54da7013dd
sanket-chamate01 <sanket.chamate2001@gmail.com>
2026-05-10 22:13:26 +0530

    section4-connect to db

---
+++ springSecurity4/src/test/java/com/mybank/SpringSecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springSecurity1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JPMorganBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
