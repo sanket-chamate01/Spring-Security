0177cd08880e39cde21631446101cb87393024df
sanket-chamate01 <sanket.chamate2001@gmail.com>
2026-05-11 22:41:30 +0530

    section4-connect to db

---
+++ springSecurity4/src/main/java/com/mybank/config/BankUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.mybank.config;
+
+import com.mybank.model.Customer;
+import com.mybank.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BankUserDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(username).orElseThrow(() ->
+                new UsernameNotFoundException("Userdetails not found for user: " + username));
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+        return new User(customer.getEmail(), customer.getPwd(), authorities);
+    }
+}
