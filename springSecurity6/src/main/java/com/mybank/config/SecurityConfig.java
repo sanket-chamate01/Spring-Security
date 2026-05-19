f1e0ea957c63bf82e42720f4a2cd0396123646ad
sanket-chamate01 <sanket.chamate2001@gmail.com>
2026-05-19 16:53:53 +0530

    section6- customizing authentication providers

---
+++ springSecurity6/src/main/java/com/mybank/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.mybank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((request) -> request
+                .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
+                .requestMatchers("/notices", "/contact", "/register", "/error").permitAll());
+        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    // in memory users created with role based access
+    // {noop} is no operation for password encoder so treat the password as plain text
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+    // below method is available by default but we added it explicitly to customize it
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+//        return new BcryptPassword4jPasswordEncoder();
+        // we can directly use above method by PasswordEncoderFactories.createDelegatingPasswordEncoder() contains all the available encoders in it and if in future we want to change encoding type then we will have to change a lot of things.
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker(){
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
